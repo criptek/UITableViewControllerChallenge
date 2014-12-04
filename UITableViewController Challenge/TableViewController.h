070c2aa493260dbe7a6d72f016b76124a5b8454d
Joan Busquet <jbusquet@MacBook-Pro-de-Joan-Busquet.local>
2014-12-04 21:22:34 +0100

    Challenge ok

--- UITableViewController Challenge/TableViewController.h
+++ UITableViewController Challenge/TableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  TableViewController.h
 //  UITableViewController Challenge
 //
 //  Created by Joan Busquet on 4/12/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface TableViewController : UITableViewController
 
 @end
-
